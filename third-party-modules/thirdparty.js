80cc4249d586006068f000ede4a441a9413a3ac0
ProgressOjubgbo22 <lingardj852@gmail.com>
2026-01-13 13:36:16 -0800

    Add files via upload

---
+++ third-party-modules/thirdparty.js
@@ -0,0 +1,12 @@
+// ================= Working with npm (Third-Party Modules) =================
+
+// Import a third-party package (example: lodash)
+const _ = require("lodash");
+
+// Using lodash to capitalize a string
+const name = "ojugbo";
+console.log("Capitalized:", _.capitalize(name)); // Ojugbo
+
+// Notes: 
+// 1. Make sure to run `npm init -y` to create package.json
+// 2. Install lodash using: npm install lodash
